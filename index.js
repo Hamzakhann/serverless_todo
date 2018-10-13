7630680b267061c37ad4c87dae083c5a16bdb33b
Hamzakhann <hamzakhann66@gmail.com>
2018-10-13 14:31:48 +0500

    add get all task and single task route

--- index.js
+++ index.js
@@ -39,68 +39,49 @@ app.post('/todo/api/v1.0/tasks', (req, res)=> {
       res.status(200).json({result : params.Item});
     }
   });
+});
 
+
+
+
+//GET METHOD
+//Get all tasks
+app.get('/todo/api/v1.0/tasks', (req, res)=> {
+  const params = {
+    TableName: USERS_TABLE,
+  }
+
+  dynamoDb.scan(params , (error , result) =>{
+    if(error){
+      res.status(400).json({ error: 'Could not get tasks' });
+    }else{
+      res.status(200).json({data : result})
+    }
+  })
 
 
 })
 
 
 
+//GET METHOD
+//Get single task
 
-
-
-// Get User endpoint
-app.get('/users/:userId', function (req, res) {
+app.get('/todo/api/v1.0/tasks/:taskId', (req, res)=> {
   const params = {
     TableName: USERS_TABLE,
     Key: {
-      userId: req.params.userId,
+      id: req.params.taskId,
     },
   }
-
-  dynamoDb.get(params, (error, result) => {
-    if (error) {
-      console.log(error);
-      res.status(400).json({ error: 'Could not get user' });
+  dynamoDb.get(params , (error , result)=>{
+    if(error){
+      res.status(400).json({ error: 'Could not get task' });
+    }else{
+      res.status(200).json({data : result})
     }
-    if (result.Item) {
-      const {userId, name} = result.Item;
-      res.json({ userId, name });
-    } else {
-      res.status(404).json({ error: "User not found" });
-    }
-  });
+  })
 })
 
-
-
-
-
-
-// Create User endpoint
-app.post('/users', function (req, res) {
-  const { userId, name } = req.body;
-  if (typeof userId !== 'string') {
-    res.status(400).json({ error: '"userId" must be a string' });
-  } else if (typeof name !== 'string') {
-    res.status(400).json({ error: '"name" must be a string' });
-  }
-
-  const params = {
-    TableName: USERS_TABLE,
-    Item: {
-      userId: userId,
-      name: name,
-    },
-  };
-
-  dynamoDb.put(params, (error) => {
-    if (error) {
-      console.log(error);
-      res.status(400).json({ error: 'Could not create user' });
-    }
-    res.json({ userId, name });
-  });
-})
 
 module.exports.handler = serverless(app);
